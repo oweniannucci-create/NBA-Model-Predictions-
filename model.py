5ccee2fa107e1179e456eff160c134da0f345bd7
wgerrish-eng <wgerrish@trinitypawling.org>
2025-10-02 14:11:17 -0400

    Loads and Calculates data

--- model.py
+++ model.py
@@ -1,1 +1,15 @@
-print(hello)
+import pandas as pd
+
+# Load your data
+df = pd.read_csv("TeamStatistics.csv")
+
+
+numeric_cols = df.select_dtypes(include="number").columns
+
+# Group by teamName and calculate averages
+team_averages = df.groupby("teamName")[numeric_cols].mean().round(2)
+
+print(team_averages.head())
+
+
+
