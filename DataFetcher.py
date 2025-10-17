c4c07c63730fa376d411598d75b33630a6c66c50
jfoggle <jrfoglesong@icloud.com>
2025-10-17 08:34:23 -0400

    update on dropped columns

--- DataFetcher.py
+++ DataFetcher.py
@@ -37,7 +37,7 @@ def get_cleaned_games_with_winner_column():
     df = pd.read_csv("Data/Games.csv")
 
     # Drop the columns you don't need
-    columns_to_drop = ["arenaId", "homeTeamCity", "awayTeamCity","seriesgamenumber","gamelabel","gamesublabel","attendance","homescore","awayscore","gameid","hometeamcity"]
+    columns_to_drop = ["arenaId", "homeTeamCity", "awayTeamCity","seriesgamenumber","gamelabel","gamesublabel","attendance","homescore","awayscore","gameid","hometeamcity","gametype"]
     df = df.drop(columns=columns_to_drop, errors="ignore")
 
     # Define a function to assign a season based on the game date
