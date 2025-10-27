2fb96fc533e4d02b8fbc80072952df80dbceb63f
stejwm <steve.jwm@gmail.com>
2025-10-26 20:12:47 -0400

    rest days function

--- DataFetcher.py
+++ DataFetcher.py
@@ -32,6 +32,29 @@ def get_average_draft_data():
     return summary_df
 
 
+def get_rest_days():
+    filename = 'Data/Games.csv'  # CSV Name
+    okc_games = []
+
+    df = pd.read_csv(filename)
+
+    df['gameDate'] = pd.to_datetime(df['gameDate'])
+
+    # Data Frame
+    home = df[['hometeamName', 'gameDate']].rename(columns={'hometeamName': 'Team'})
+    away = df[['awayteamName', 'gameDate']].rename(columns={'awayteamName': 'Team'})
+    all_games = pd.concat([home, away])
+
+    all_games = all_games.sort_values(by=['Team', 'gameDate'])
+
+    # Intervals
+    all_games['RestDays'] = all_games.groupby('Team')['gameDate'].diff().dt.days
+
+    output_path = 'Data/Team_RestDays.csv'
+    all_games.to_csv(output_path, index=False)
+
+    return all_games
+
 def get_cleaned_games_with_winner_column():
     # Load your dataset
     df = pd.read_csv("Data/Games.csv")
--- Main.py
+++ Main.py
@@ -11,6 +11,7 @@ from pandasgui import show
 games_df = DataFetcher.get_cleaned_games_with_winner_column()
 stats_df = DataFetcher.get_teamaveragestatistics_from_year()
 draft_df = DataFetcher.get_average_draft_data()
+rest_days = DataFetcher.get_rest_days()
 
 print(games_df.info())
 print(stats_df.info())
